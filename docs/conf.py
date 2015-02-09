24a3beb039c93d7d7c3752c91ad665602571caf1
Augusto Almeida <acba@cin.ufpe.br>
2015-02-09 00:41:34 -0300

    Fix changes from sphinx 1.3 to 1.2

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
-              'sphinx.ext.napoleon', 'sphinx.ext.mathjax']
+              'sphinxcontrib.napoleon', 'sphinx.ext.mathjax']
 
 
 # Napoleon settings
